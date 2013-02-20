72820cab0f8ae58fc2f0398b527910eddf071651
Anum Qudsia <anumqudsia@hotmail.com>
2013-02-20 17:05:04 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webside::Application.config.secret_token = '44c3ae95f31df78e013e828c4aebb2e9d14620870bc6dae1436576d254d08abd3bb1c3c176bba7c29b6c7f4a2baa04c48dda5fdf2dff3ecc0c4d8a8994ef5a74'
